cab3d25b04ec1e6ea7c1dedea188dc9fdb9de57c
Tsyrzhima <sss1992bur@mail.ru>
2025-04-08 22:17:41 +0800

    Реализовывает оформление заказа через транзакцию, контейнер rabbitmq

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -3,13 +3,12 @@
 namespace App\Services;
 
 use App\DTO\OrderCreateDTO;
-use App\Http\Requests\HandleCheckoutOrderRequest;
 use App\Models\Order;
 use App\Models\OrderProduct;
 use App\Models\User;
 use App\Models\UserProduct;
 use Illuminate\Support\Facades\Auth;
-use App\Services\CartService;
+use Illuminate\Support\Facades\DB;
 
 class OrderService
 {
@@ -20,27 +19,36 @@ class OrderService
     }
     public function create(OrderCreateDTO $data)
     {
-
         /** @var User $user */
         $user = Auth::user();
         $sum = $this->cartService->getSum($user);
         $userProducts = $user->userProducts()->get();
-        $order = Order::create([
-            'user_id' => $user->id,
-            'contact_name' => $data['contact_name'],
-            'contact_phone' => $data['contact_phone'],
-            'comment' => $data['comment'],
-            'address' => $data['address'],
-        ]);
-        $orderId = $order->id;
-        foreach ($userProducts as $userProduct) {
-            OrderProduct::create([
-                'order_id' => $orderId,
-                'product_id' => $userProduct->product_id,
-                'amount' => $userProduct->amount,
+
+        DB::beginTransaction();
+        try{
+            $order = Order::create([
+                'user_id' => $user->id,
+                'contact_name' => $data->getName(),
+                'contact_phone' => $data->getPhone(),
+                'comment' => $data->getComment(),
+                'address' => $data->getAddress(),
             ]);
+            $orderId = $order->id;
+            foreach ($userProducts as $userProduct) {
+                OrderProduct::create([
+                    'order_id' => $orderId,
+                    'product_id' => $userProduct->product_id,
+                    'amount' => $userProduct->amount,
+                ]);
+            }
+            UserProduct::query()->where('user_id', $user->id)->delete();
+
+            DB::commit();
+        }catch (\Throwable $exception){
+            DB::rollBack();
+
+            throw $exception;
         }
-        UserProduct::query()->where('user_id', $user->id)->delete();
     }
 
 }
